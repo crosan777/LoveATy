a1a5bd907af4dff1b7216c4d8e43afee9a586fec
poli3627 <71379732+poli3627@users.noreply.github.com>
2025-05-08 18:53:39 +0200

    Initial Display doesn't work properly

---
+++ Assets/Script/Characters/Character.cs
@@ -0,0 +1,44 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Character
+{
+    #region Atributes
+    public string Name { get; set; }
+    public string Surname { get; set; }
+    public string Surname2 { get; set; }
+
+    public string Description { get; set; }
+
+    public int Age = 18;
+
+    public enum ActivityType { Activo, Tranquilo } 
+    public enum MethodologyType { Despreocupado, Espontaneo, Metodico }
+    public enum SocialType { Extrovertido, Introvertido, Neutral }
+    public enum TraditionType { Clasico , Moderno }
+    public enum RelationshipType { Romantico, Picante, Tierno, Evitativo }
+    
+
+    public ActivityType Activity;
+    public MethodologyType Methodology;
+    public SocialType Social;
+    public TraditionType Tradition;
+    public RelationshipType Relationship;
+
+    #endregion
+
+    public Character (string name, string surname, string surname2, string Description, 
+          ActivityType activity, MethodologyType methodology,
+          SocialType social, TraditionType tradition,
+          RelationshipType relationship) // Constructor for characters
+    {
+        Name = name; //Name
+        Surname = surname; //Surname
+        Surname2 = surname2; //Surname2
+        Activity = activity; //Activity type
+        Methodology = methodology; //Methodology Type
+        Social = social; //Social Type
+        Tradition = tradition; //Tradition type
+        Relationship = relationship; //Relationship type
+    }
+}
---
+++ Assets/Script/Characters/Client.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class Client : Character
+{
+    public Client(string name, string surname, string surname2, string Description,
+           ActivityType activity, MethodologyType methodology,
+           SocialType social, TraditionType tradition,
+           RelationshipType relationship) : base (name, surname, surname2, Description,
+           activity, methodology, social, tradition, relationship)
+    {
+        Name = name; //Name
+        Surname = surname; //Surname
+        Surname2 = surname2; //Surname2
+        Activity = activity; //Activity type
+        Methodology = methodology; //Methodology Type
+        Social = social; //Social Type
+        Tradition = tradition; //Tradition type
+        Relationship = relationship; //Relationship type
+    }
+}
