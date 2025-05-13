9cf49fcefb3875f9abdfb33432516e10749d9c1e
crosan777 <cristiidasti@gmail.com>
2025-05-13 16:32:29 +0200

    Squashed commit of the following:
    commit 19da63ed024c74404f6c3e865abdd5eeb31c9ef5
    Author: poli3627 <71379732+poli3627@users.noreply.github.com>
    Date:   Tue May 13 00:48:56 2025 +0200
    
        Love Agency Tycoon  V.0.1.2F2 - Bacherlor Display
    
        - Added prefab to work with the bachelors
        - Bachelor Display and Bachelor manager can be changed for the prefabs in the unity editor
        - Create a list of <gameObjects>
        - Changed name of Clients to Bachelor in all the scripts finally
    
    commit 8c3e1b8a55a067a90220ea97726be0dc15afc548
    Author: poli3627 <71379732+poli3627@users.noreply.github.com>
    Date:   Mon May 12 23:38:03 2025 +0200
    
        Love Agency Tycoon V.0.1.2F
    
        - Fixed an error
    
    commit ae04a0d3859ce69ee0d24ba53d9bbf19bb4f12a6
    Author: poli3627 <71379732+poli3627@users.noreply.github.com>
    Date:   Sun May 11 23:11:41 2025 +0200
    
        Love Agency Tycoon V.0.1.1 - Bachelor Display
    
        - Changed the name of MainMenu script to SceneManager
        - New script added BachelorDisplay
        - New script added BachelorManager
        - The description of the Bachelors contains now the questions and the responses in a single string text. This should be changed later.
    
    commit 83a9854349326de188059f69d9665c50d159531d
    Author: poli3627 <71379732+poli3627@users.noreply.github.com>
    Date:   Thu May 8 19:25:32 2025 +0200
    
        Love Agency Tycoon V.0.1 - Bachelor Display
    
        - Implemented Menu and input system
        to track buttons.
        - New Scene Test to test things
        - Lots of bugs found
    
    commit a1a5bd907af4dff1b7216c4d8e43afee9a586fec
    Author: poli3627 <71379732+poli3627@users.noreply.github.com>
    Date:   Thu May 8 18:53:39 2025 +0200
    
        Initial Display doesn't work properly

---
+++ Assets/Script/Characters/BachelorManager.cs
@@ -0,0 +1,46 @@
+using Mono.Cecil.Cil;
+using System.Collections.Generic;
+using UnityEngine;
+using static BachelorDisplay;
+using System;
+
+public class BachelorManager : MonoBehaviour
+{
+    public List<Bachelor> Bachelors = new List<Bachelor>();
+    
+    private void BachelorList (List<Bachelor> Bachelors)
+    {
+            new Bachelor ("Ricard", "Perea", "Ros",
+                "- Tu pareja quiere apuntarse a bailes de salón, tú... Pienso que me está tomando el peloç" +
+                "Cuando miras la tele... Miro mi programa favorito y despúes apago" +
+                "En una fiesta eres... el alma de la fiesta" +
+                "¿Qué tipo de juegos prefieres? Los Videojuegos" +
+                "Cuando compras una cama, tienes en cuenta...Lo bien que dormire en ella yo y mi pareja", 18,
+                Bachelor.ActivityType.Tranquilo,
+                Bachelor.MethodologyType.Metodico,
+                Bachelor.SocialType.Extrovertido,
+                Bachelor.TraditionType.Moderno,
+                Bachelor.RelationshipType.Romantico);
+            
+    }
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        for (int i = 0; i < 1; i++)
+        {
+        
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void UpdateUI()
+    {
+
+    }
+}
