53dd7ea24c6dd6de8e77ae7044d7d9ea9d56a06e
crosan777 <cristiidasti@gmail.com>
2025-05-14 18:03:48 +0200

    Candidate Creator Code

--- Assets/Script/Characters/CharacterInfo.cs
+++ Assets/Script/Characters/CharacterInfo.cs
@@ -1,7 +1,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public abstract class Character : MonoBehaviour
+public enum ActivityType { Activo, Tranquilo } 
+public enum MethodologyType { Despreocupado, Espontaneo, Metodico }
+public enum SocialType { Extrovertido, Introvertido, Neutral }
+public enum TraditionType { Clasico , Moderno }
+public enum RelationshipType { Romantico, Picante, Tierno, Evitativo }
+
+public enum GenderType { Male, Female }
+
+public class Character : MonoBehaviour
 {
     #region Atributes
     public string Name { get; set; } 
@@ -12,12 +20,7 @@ public abstract class Character : MonoBehaviour
 
     public int Age = 18;
 
-    public enum ActivityType { Activo, Tranquilo } 
-    public enum MethodologyType { Despreocupado, Espontaneo, Metodico }
-    public enum SocialType { Extrovertido, Introvertido, Neutral }
-    public enum TraditionType { Clasico , Moderno }
-    public enum RelationshipType { Romantico, Picante, Tierno, Evitativo }
-    
+    public GenderType Gender;
 
     public ActivityType Activity;
     public MethodologyType Methodology;
@@ -25,20 +28,7 @@ public abstract class Character : MonoBehaviour
     public TraditionType Tradition;
     public RelationshipType Relationship;
 
-    #endregion
 
-    public Character (string name, string surname, string surname2, string Description, 
-          ActivityType activity, MethodologyType methodology,
-          SocialType social, TraditionType tradition,
-          RelationshipType relationship) // Constructor for characters
-    {
-        Name = name; //Name
-        Surname = surname; //Surname
-        Surname2 = surname2; //Surname2
-        Activity = activity; //Activity type
-        Methodology = methodology; //Methodology Type
-        Social = social; //Social Type
-        Tradition = tradition; //Tradition type
-        Relationship = relationship; //Relationship type
-    }
+
+    #endregion
 }
