3aee269adc39e63bce30d02841e70fb5c105a1d9
crosan777 <cristiidasti@gmail.com>
2025-05-14 15:11:36 +0200

    Candidate Display Start

---
+++ Assets/Script/Characters/CandidateDisplay.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class CandidateDisplay : MonoBehaviour
+{
+
+    public enum ActivityType { Activo, Tranquilo }
+    public enum MethodologyType { Despreocupado, Espontaneo, Metodico }
+    public enum SocialType { Extrovertido, Introvertido, Neutral }
+    public enum TraditionType { Clasico, Moderno }
+    public enum RelationshipType { Romantico, Picante, Tierno, Evitativo }
+
+    public void RandomAtributes()
+    {
+        Random.UnityEngi alea;
+
+        UnityEngine.enum randomEnum
+     }
+
+    public void getCandidateInfo()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
