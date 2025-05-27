5bd4a6705619e723c0ea7362e0c5fef9a5820713
poli3627 <71379732+poli3627@users.noreply.github.com>
2025-05-27 16:08:33 +0200

    Added new Bachelors

--- Assets/Scripts/Characters Scripts/Pick Bachelors.cs
+++ Assets/Scripts/Characters Scripts/Pick Bachelors.cs
@@ -1,7 +1,12 @@
+using NUnit.Framework;
+using System.Collections.Generic;
 using UnityEngine;
+
 
 public class PickBachelors : MonoBehaviour
 {
+    List<CharacterInfo> Bachelors = CharacterInfo[];
+    Bachelors = CharacterInfo[]; 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
