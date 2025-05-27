dc9a5f12cf3d599aefbcec7275ebefcef2c7cb4e
crosan777 <cristiidasti@gmail.com>
2025-05-27 16:44:42 +0200

    bachelor

---
+++ Assets/Script/HUD/BachelorDisplay.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class BachelorDisplay : MonoBehaviour
+{
+    
+    
+    public int index;
+    public CharacterInfo[] CurrentCharacterInfo;
+    public Image BachelorPortrait;
+    public TMP_Text BachelorName;
+    public TMP_Text BachelorAge;
+    public TMP_Text BachelorDescription;
+
+    public void Start()
+    {
+        DisplayBachelorInformation();
+    }
+    public void DisplayBachelorInformation()
+    {
+        BachelorPortrait.sprite = CurrentCharacterInfo[index].image;
+        BachelorName.text = CurrentCharacterInfo[index].name;
+        BachelorDescription.text = CurrentCharacterInfo[index].Description;
+        BachelorAge.text = CurrentCharacterInfo[index].Age.ToString();
+    }
+    public void RightArrow()
+    {
+        index = (index + 1)%CurrentCharacterInfo.Length; //Formula to make the index be in the List length always
+        DisplayBachelorInformation();
+    }
+}
+
