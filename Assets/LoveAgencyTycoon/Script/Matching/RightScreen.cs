933e8d707a0399d6c3aec7882a5093aa99d63383
crosan777 <cristiidasti@gmail.com>
2025-06-03 16:05:41 +0200

    Names 3rd Screen

--- Assets/LoveAgencyTycoon/Script/Matching/RightScreen.cs
+++ Assets/LoveAgencyTycoon/Script/Matching/RightScreen.cs
@@ -12,8 +12,8 @@ public class RightScreen : MonoBehaviour
     public CandidateDisplayUI candidateDisplay;
 
 
-    public Image bachelorImageUI;
-    public Image candidateImageUI;
+    public TMP_Text bachelorNameText;
+    public TMP_Text candidateNameText;
     public Image compatibilityWheel;
 
 
@@ -21,62 +21,65 @@ public class RightScreen : MonoBehaviour
     private CharacterInfo currentBachelor;
     private CharacterInfo selectedCandidate;
 
+
+    //Para detetcar el candidate
+    public void SelectedCandidate(CharacterInfo candidate)
+    {
+        selectedCandidate = candidate;
+        candidateNameText.text = selectedCandidate.Name;
+
+    }
+
+
+
     // boton MATCH
-    public void OnMatchButtonPressed()
+    public void MatchButton()
     {
         currentBachelor = bachelorDisplay.GetCurrentBachelor();
         //selectedCandidate = menuSelect.selectedCandidate;
 
         if (currentBachelor == null || selectedCandidate == null) return;
 
-        ShowPortraits();
-        ShowCompatibility();
+        Name();
+        CompatibilityDisplay();
     }
 
-    private void ShowPortraits()
+    private void Name()
     {
-        bachelorImageUI.sprite = currentBachelor.image;
-        candidateImageUI.sprite = selectedCandidate.image;
+        bachelorNameText.text = currentBachelor.Name;
+        candidateNameText.text =  selectedCandidate.Name;
     }
 
-    private void ShowCompatibility()
+
+    private void CompatibilityDisplay()
     {
-        float compatibility = CalculateCompatibility(currentBachelor, selectedCandidate);
+        float compatibility = CalculateCompatsibility(currentBachelor, selectedCandidate);
 
         compatibilityWheel.fillAmount = compatibility;
         compatibilityWheel.color = Color.Lerp(Color.red, Color.green, compatibility);
         // ahora lo hace instantaneo despues le añadimos alguna animacion
     }
 
-    private float CalculateCompatibility(CharacterInfo a, CharacterInfo b)
+    private float CalculateCompatsibility(CharacterInfo bachelor, CharacterInfo candidate)
     {
         int matches = 0;
 
-        if (a.Social == b.Social)
+        if (bachelor.Social == candidate.Social)
             matches++;
 
-        if (a.Methodology == b.Methodology)
+        if (bachelor.Methodology == candidate.Methodology)
             matches++;
 
-        if (a.Relationship == b.Relationship)
+        if (bachelor.Relationship == candidate.Relationship)
             matches++;
 
-        if (a.Tradition == b.Tradition)
+        if (bachelor.Tradition == candidate.Tradition)
             matches++;
 
-        if (a.Activity == b.Activity)
+        if (bachelor.Activity == candidate.Activity)
             matches++;
 
         //para la ruleta 0.0 o 1.0
         return matches / 5f;
     }
-
-
-
-    //Para detetcar el candidate
-    public void SetSelectedCandidate(CharacterInfo candidate)
-    {
-        selectedCandidate = candidate;
-    }
-
 }
