64aa6c4aaa40d393673437afaf22c5adbdcd0900
crosan777 <cristiidasti@gmail.com>
2025-06-03 17:56:42 +0200

    StartingTutoRobot

---
+++ Assets/LoveAgencyTycoon/Script/Tutorial/RobotGuideManager.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RobotGuideManager : MonoBehaviour
+{
+    public TMP_Text dialogueText;
+    public GameObject robotUI;
+
+    private int step = 0;
+    private bool actionDone = false;
+
+    private void Start()
+    {
+        StartCoroutine(RunTutorial());
+    }
+
+    public void ActionDone()
+    {
+        actionDone = true;
+    }
+
+    private IEnumerator RunTutorial()
+    {
+        robotUI.SetActive(true);
+        dialogueText.text = "¡Hola! Cambia el bachelor usando las flechas.";
+
+        yield return new WaitUntil(() => actionDone);
+        actionDone = false;
+
+        dialogueText.text = "¡Perfecto! Ahora cambia su color.";
+        yield return new WaitUntil(() => actionDone);
+        actionDone = false;
+
+        dialogueText.text = "¡Increíble! Has completado la primera parte.";
+        yield return new WaitForSeconds(2);
+
+        robotUI.SetActive(false);
+    }
+}
+
